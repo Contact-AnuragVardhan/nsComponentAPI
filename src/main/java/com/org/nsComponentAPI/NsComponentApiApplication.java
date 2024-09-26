31d4b5ae3574dd24c4d5ec19cfb796bcb0d90996
contact-anurag <contact.anuragvardhan@gmail.com>
2024-09-26 18:01:59 -0400

    first commit

---
+++ src/main/java/com/org/nsComponentAPI/NsComponentApiApplication.java
@@ -0,0 +1,13 @@
+package com.org.nsComponentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsComponentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsComponentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/nsComponentAPI/NsComponentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.nsComponentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsComponentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
